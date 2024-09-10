b81db24e4a2f04e33bf54591e467208fbd8f3dc4
Florencia <flordani98@gmail.com>
2024-09-09 23:46:47 -0300

    implementa logica de juego utilizando useState y useEffect

--- src/GameApp.jsx
+++ src/GameApp.jsx
@@ -1,28 +1,78 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 
 
-const score = 20;
-const highscore = 20;
+
+const MAX_SCORE = 20;
+const MIN_SCORE = 0;
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
 
 const message = "mensaje de tabla";
 
 export const GameApp = () => {
 
     const [value, setValue] = useState(["?"])
+    
+    const [winningNumber, setWinningNumber] = useState(generateRandomNumber());
+    
+    const [score, setScore] = useState(MAX_SCORE);
+    
+    const [highscore, setHighscore] = useState(0);
+
+    const [message, setMessage] = useState('');
 
     const handleInputChange = ({target}) =>{
         setValue(target.value)
     }
 
     const handleSubmit = ( e ) =>{
-        e.preventDefault
+        e.preventDefault();
         // setValue('')
+        if (value === winningNumber) {
+            setMessage('¡Número correcto!');
+            setScore((prevScore) => Math.min(prevScore + 10, MAX_SCORE)); 
+            setWinningNumber(generateRandomNumber()); 
+        } else if(value > winningNumber){
+            setMessage('Muy alto!');
+            setScore((prevScore) => prevScore - 1); 
+        }
+        else{
+            setMessage('Muy bajo!');
+            setScore((prevScore) => prevScore - 1); 
+        }
+        
+        console.log(winningNumber)
+    
     }
+
+    const handleRestart= () => {
+        setScore(MAX_SCORE);
+        setWinningNumber(generateRandomNumber());
+        setMessage('');
+        setValue('');
+
+    }
+
+       
+    useEffect(() => {
+        if (score <= MIN_SCORE) {
+            setMessage('¡Game Over!');
+            if (score > highscore) {
+                setHighscore(score);
+            }
+            setScore(MAX_SCORE); 
+            setWinningNumber(generateRandomNumber()); 
+        }
+    }, [score, highscore]);
+    
+
 
   return (
     <div className='app-container'>
         <header>
-            <button id='restart-button' className='button'>Reiniciar</button>
+            <button id='restart-button' className='button' onClick={handleRestart}>Reiniciar</button>
             <h1>¡Adivinar el Número!</h1>
 
         </header>
@@ -54,8 +104,8 @@ export const GameApp = () => {
                     <td>{ message }</td>
                 </tr>
                 <tr className='info-score'>
-                    <td> {highscore} </td>
-                    <td>{score} </td>
+                    <td> Score: {highscore} </td>
+                    <td> Highscore: {score} </td>
                 </tr>
             </tbody>
         </table>
